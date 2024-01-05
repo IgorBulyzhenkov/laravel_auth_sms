824b9f2a177887b38b453c4f0fea14a48c7aa5b1
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2024-01-05 17:01:18 +0200

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
         <!-- <style>body{opacity: 0;}</style> -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        <link
         <link rel="shortcut icon" href="favicon.ico">
         <!-- <meta name="robots" content="noindex, nofollow"> -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
